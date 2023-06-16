066ed2e1ea991e60f3b3f0ba6ebc5ea1f35dd5c0
rianmz-genz <adrianji1945@gmail.com>
2023-06-16 15:20:46 +0700

    card for faq

---
+++ components/atoms/FaqCard.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import Text from './Text'
+import { FiPlus, FiX } from 'react-icons/fi'
+
+const FaqCard = ({question, answer}) => {
+    const [isOpened, setIsOpened] = useState(false)
+  return (
+    <li className='rounded-lg border border-black/10 p-4 transition-all duration-300'>
+        <div className='flex justify-between'>
+            <Text size={'base'} weight={'bold'}>{question}</Text>
+            <button onClick={()=> setIsOpened(!isOpened)}>
+                <FiPlus className={`${isOpened ? "rotate-45" :"rotate-0"} text-lg tex-dark transition-all duration-300`} />
+            </button>
+        </div>
+        <div className={`${isOpened ? "h-fit visible opacity-100" : "h-0 invisible opacity-0"} transition-all duration-300`}>
+            <Text size={'small'}>{answer}</Text>
+        </div>
+    </li>
+  )
+}
+
+export default FaqCard
