3e8952b161473475b65116f5ad5104106a4a3a95
rianmz-genz <adrianji1945@gmail.com>
2023-06-17 22:25:07 +0700

    add silver variant

--- components/atoms/Button.js
+++ components/atoms/Button.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { variant } from '@/utils/utils';
-const Button = ({ color, size, isSquare = false, ...props}) => {
-    const button = variant(`${isSquare ? "rounded-md" : "rounded-full"}`, {
+const Button = ({ color, size, additionals,isSquare = false, ...props}) => {
+    const button = variant(`${isSquare ? "rounded-md" : "rounded-full"} ${additionals}`, {
         color: {
           gradient: 'bg-gradient-to-r from-oren to-kuning text-white hover:scale-95',
+          silver: 'bg-silver/20 text-black',
           white: 'bg-white text-softyellow hover:bg-kuning hover:text-white',
           orentransparent: 'bg-oren/10 text-oren hover:shadow-lg',
           oren: 'bg-oren text-white hover:scale-95 ttransition-all duration-300'
         },
         size: {
           sm: 'px-5 py-1 text-sm',
+          base: 'px-4 py-2',
           md: 'px-8 py-2 font-semibold',
           lg: 'px-10 py-3 text-lg font-bold',
         },
