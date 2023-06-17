7f3e5426c638da773aba112700836d8ca5a28991
rianmz-genz <adrianji1945@gmail.com>
2023-06-17 21:07:01 +0700

    make competition details with spa

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
-import { AboutSection, CompetitionSection, FaqSection, Footer, HeroSection, SkemaSection, TimelineSection } from '@/components'
+import { AboutSection, CompetitionDetails, CompetitionSection, FaqSection, Footer, HeroSection, SkemaSection, TimelineSection } from '@/components'
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <main>
-      <Head>
-        <title>IITC</title>
-        <meta name="title" content='IITC' />
-      </Head>
-      <HeroSection />
-      <AboutSection />
-      <SkemaSection />
-      <CompetitionSection />
-      <TimelineSection />
-      <FaqSection />
-      <Footer/>
-    </main>
-  )
+  const [isCompetitionDetail, setIsCompetitionDetail] = useState(false)
+  const [competitionName, setCompetitionName] = useState("")
+  return isCompetitionDetail
+    ? <CompetitionDetails
+        competitionName={competitionName}
+        setIsCompetitionDetail={setIsCompetitionDetail}
+      />
+    : <main>
+        <Head>
+          <title>IITC</title>
+          <meta name="title" content='IITC' />
+        </Head>
+        <HeroSection />
+        <AboutSection />
+        <SkemaSection />
+        <CompetitionSection
+          setCompetitionName={setCompetitionName}
+          setIsCompetitionDetails={setIsCompetitionDetail}
+        />
+        <TimelineSection />
+        <FaqSection />
+        <Footer/>
+      </main>
+  
 }
