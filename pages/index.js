e52cabe56830a057f2bad5e07e075fdaffa0faf3
rianmz-genz <adrianji1945@gmail.com>
2023-06-17 08:20:01 +0700

    add footer section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { AboutSection, CompetitionSection, FaqSection, HeroSection, SkemaSection, TimelineSection } from '@/components'
+import { AboutSection, CompetitionSection, FaqSection, Footer, HeroSection, SkemaSection, TimelineSection } from '@/components'
 import Head from 'next/head'
 
 export default function Home() {
@@ -14,6 +14,7 @@ export default function Home() {
       <CompetitionSection />
       <TimelineSection />
       <FaqSection />
+      <Footer/>
     </main>
   )
 }
