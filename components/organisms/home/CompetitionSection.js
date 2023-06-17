d0b9246aeeb0aead67c9eb9e374dcb5d10cca4c9
rianmz-genz <adrianji1945@gmail.com>
2023-06-17 21:09:06 +0700

    implementation spa competition detail

--- components/organisms/home/CompetitionSection.js
+++ components/organisms/home/CompetitionSection.js
@@ -5,7 +5,7 @@ import ParagraphSection from '@/components/molecules/ParagraphSection'
 import Image from 'next/image'
 import React from 'react'
 import { HiOutlineUsers } from 'react-icons/hi'
-const CompetitionSection = () => {
+const CompetitionSection = ({setIsCompetitionDetails, setCompetitionName}) => {
   return (
     <section id='lomba' className='py-12 flex justify-center'>
       <div className='w-11/12 mx-auto'>
@@ -15,31 +15,41 @@ const CompetitionSection = () => {
         description={'pilih lomba yang sesuai dengan kemampuan kamu ya..'}
         />
       <ol className='rounded-xl w-full mt-12 grid grid-cols-5 gap-3'>
-        <CompetitonCard 
+        <CompetitonCard
+            setCompetitionName={setCompetitionName}
+            setIsCompetitionDetail={setIsCompetitionDetails} 
             category={'Pelajar & Mahasiswa'}
             imgSrc={'/images/trex.png'}
             maxMembers={5}
             title={'Website Design'}
         />
-        <CompetitonCard 
+        <CompetitonCard
+            setCompetitionName={setCompetitionName} 
+            setIsCompetitionDetail={setIsCompetitionDetails}
             category={'Pelajar & Mahasiswa'}
             imgSrc={'/images/gajah.png'}
             maxMembers={5}
             title={'Business Plan'}
         />
-        <CompetitonCard 
+        <CompetitonCard
+            setCompetitionName={setCompetitionName}
+            setIsCompetitionDetail={setIsCompetitionDetails} 
             category={'Pelajar & Mahasiswa'}
             imgSrc={'/images/koala.png'}
             maxMembers={5}
             title={'E-Sport'}
         />
-        <CompetitonCard 
+        <CompetitonCard
+            setCompetitionName={setCompetitionName}
+            setIsCompetitionDetail={setIsCompetitionDetails} 
             category={'Pelajar & Mahasiswa'}
             imgSrc={'/images/trex.png'}
             maxMembers={5}
             title={'Cover Music'}
         />
-        <CompetitonCard 
+        <CompetitonCard
+            setCompetitionName={setCompetitionName} 
+            setIsCompetitionDetail={setIsCompetitionDetails}
             category={'Pelajar & Mahasiswa'}
             imgSrc={'/images/trex.png'}
             maxMembers={5}
