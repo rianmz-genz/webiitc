3f8873e753d51cc8fc5703687d41dd783c592ffd
rianmz-genz <adrianji1945@gmail.com>
2023-06-17 22:25:22 +0700

    back

--- components/organisms/details/CompetitionDetails.js
+++ components/organisms/details/CompetitionDetails.js
@@ -1,16 +1,32 @@
 import Head from 'next/head'
 import React from 'react'
-
+import Footer from '../home/Footer'
+import { IoMdArrowBack } from 'react-icons/io'
+import Button from '@/components/atoms/Button'
+import Link from 'next/link'
 const CompetitionDetails = ({setIsCompetitionDetail, competitionName}) => {
     return (
     <>
-    <Head>
-      <title>Detail</title>
-      <meta name="title" content='IITC' />
-    </Head>
-    <main>
-        {competitionName}
-    </main>
+        <Head>
+          <title>Detail</title>
+          <meta name="title" content='IITC' />
+        </Head>
+        <main>
+            <div className='w-11/12 mx-auto py-6'>
+                <Button onClick={()=> setIsCompetitionDetail(false)} additionals={'flex items-center'} color={'silver'} size={'base'}>
+                        <IoMdArrowBack className='text-lg cursor-pointer mr-2' /> 
+                        Back
+                </Button>
+                <ul className='flex space-x-3 mt-8'>
+                    <li>Competitions</li>
+                    <li>/</li>
+                    <li>Detail</li>
+                    <li>/</li>
+                    <li>{competitionName}</li> 
+                </ul>
+            </div>
+            <Footer />
+        </main>
     </>
   )
 }
