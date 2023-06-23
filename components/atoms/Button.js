884c0eaac4a49036c05b266d249d679f9e2efb2f
rianmz-genz <adrianji1945@gmail.com>
2023-06-23 09:40:00 +0700

    add variant button

--- components/atoms/Button.js
+++ components/atoms/Button.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { variant } from '@/utils/utils';
+import React from "react";
+import { variant } from "@/utils/utils";
 const Button = ({ color, size, additionals, isSquare = false, ...props }) => {
   const button = variant(
-    `${isSquare ? 'rounded-md' : 'rounded-full'} ${additionals}`,
+    `${isSquare ? "rounded-md" : "rounded-full"} ${additionals}`,
     {
       color: {
+        gradient2:
+          "bg-gradient-to-br from-[#FFCC49] to-[#F37F00] text-white hover:scale-95",
         gradient:
-          'bg-gradient-to-r from-oren to-kuning text-white hover:scale-95',
-        silver: 'bg-silver/20 text-black',
-        white: 'bg-white text-softyellow hover:bg-kuning hover:text-white',
-        orentransparent: 'bg-oren/10 text-oren hover:shadow-lg',
-        oren: 'bg-oren text-white hover:scale-95 ttransition-all duration-300',
+          "bg-gradient-to-r from-oren to-kuning text-white hover:scale-95",
+        silver: "bg-silver/20 text-black",
+        white: "bg-white text-softyellow hover:bg-kuning hover:text-white",
+        orentransparent: "bg-oren/10 text-oren hover:shadow-lg",
+        oren: "bg-oren text-white hover:scale-95 ttransition-all duration-300",
       },
       size: {
-        sm: 'px-5 py-1 text-sm',
-        base: 'px-4 py-2',
-        md: 'px-8 py-2 font-semibold',
-        lg: 'px-10 py-3 text-lg font-bold',
+        sm: "px-5 py-1 text-sm",
+        base: "px-4 py-2",
+        md: "px-8 py-2 font-semibold",
+        lg: "px-10 py-3 text-lg font-bold",
       },
     }
   );
@@ -28,7 +30,7 @@ const Button = ({ color, size, additionals, isSquare = false, ...props }) => {
   );
 };
 Button.defaultProps = {
-  color: 'gradient',
-  size: 'md',
+  color: "gradient",
+  size: "md",
 };
 export default Button;
