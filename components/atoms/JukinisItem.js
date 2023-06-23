f0163d9ab5510955e142f2e64656706eaa344541
rianmz-genz <adrianji1945@gmail.com>
2023-06-24 00:04:15 +0700

    width juknis

--- components/atoms/JukinisItem.js
+++ components/atoms/JukinisItem.js
@@ -1,24 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import Text from './Text'
+import React, { useEffect, useState } from "react";
+import Text from "./Text";
 
-const JukinisItem = ({ w, children }) => {
-    const [widthnya, setWidthnya] = useState("")
-    useEffect(() => {
-        setWidthnya(`w-[${w}%]`)
-        console.log(widthnya)
-    },[w, widthnya])
-    
+const JuknisItem = ({ w, children }) => {
   return (
-    <li className='flex w-full'>
-      <Text additionals={'w-6/12'}>{children}</Text>
-      <div className='flex w-6/12 items-center'>
+    <li className="flex w-full">
+      <Text additionals={"w-6/12"}>{children}</Text>
+      <div className="flex w-6/12 items-center">
         <div className={`w-full h-3 mr-3 bg-silver/40 rounded-full flex`}>
-          <div className={`${widthnya} bg-oren h-3 rounded-full`}></div>
+          <div
+            style={{ width: `${w}%` }}
+            className={` bg-oren h-3 rounded-full`}
+          ></div>
         </div>
-        <Text color={'black'}>{w}%</Text>
-      </div>    
+        <Text color={"black"}>{w}%</Text>
+      </div>
     </li>
   );
-}
+};
 
-export default JukinisItem
+export default JuknisItem;
