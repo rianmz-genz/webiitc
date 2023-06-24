8d223dcb285eb1f06be0f3cad216739671b2b237
rianmz-genz <adrianji1945@gmail.com>
2023-06-24 16:56:42 +0700

    login implementation

---
+++ api/auth/LoginApi.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { urlLogin } from "../routes/auth";
+
+const LoginApi = async ({ email, password }) => {
+  try {
+    const data = { email, password };
+    const res = await axios({
+      baseURL: urlLogin,
+      method: "POST",
+      data,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+
+export default LoginApi;
