34391b48867ca42a604e77d69a96be53093ad0ed
rianmz-genz <adrianji1945@gmail.com>
2023-06-24 16:57:23 +0700

    login

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,32 @@
+import LoginApi from "@/api/auth/LoginApi";
 import { Button } from "@/components";
 import Text from "@/components/atoms/Text";
 import InputTitle from "@/components/molecules/InputTitle";
 import AuthPage from "@/components/pagetemplate/AuthPage";
+import axios from "axios";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isHitApi, setIsHitApi] = useState(false);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setIsHitApi(true);
+    LoginApi({ email, password })
+      .then((res) => {
+        console.log(res);
+        setIsHitApi(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsHitApi(false);
+      });
+  };
   return (
-    <AuthPage title={"Daftar IITC"}>
+    <AuthPage onSubmit={handleLogin} title={"Daftar IITC"}>
       <Text size={"mdtitle"} weight={"bold"}>
         Sign In
       </Text>
@@ -37,7 +54,13 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <Button color={"gradient2"}>Submit</Button>
+      <Button color={"gradient2"}>
+        {isHitApi ? (
+          <AiOutlineLoading3Quarters className="text-white text-xl animate-spin" />
+        ) : (
+          "Submit"
+        )}
+      </Button>
     </AuthPage>
   );
 };
