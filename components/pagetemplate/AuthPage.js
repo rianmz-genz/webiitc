653129cabc96fec0cd646a4ed8f66dc02fc8ab76
rianmz-genz <adrianji1945@gmail.com>
2023-06-24 16:57:16 +0700

    responsive and login implementation

--- components/pagetemplate/AuthPage.js
+++ components/pagetemplate/AuthPage.js
@@ -2,16 +2,18 @@ import React from "react";
 import Container from "../molecules/Container";
 import Head from "next/head";
 
-const AuthPage = ({ children, title }) => {
+const AuthPage = ({ children, title, onSubmit }) => {
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
       <Container>
-        <main className="w-full pr-3 pl-12 mx-auto justify-between flex items-center min-h-screen ">
-          <form className="w-7/12 pr-12">{children}</form>
-          <aside className="w-5/12 min-h-screen py-4 relative">
+        <main className="w-full lg:flex-row flex-col lg:py-0 py-12 lg:pr-3 lg:pl-12 mx-auto justify-between flex items-center min-h-screen ">
+          <form onSubmit={onSubmit} className="w-11/12 lg:w-7/12 lg:pr-12">
+            {children}
+          </form>
+          <aside className="w-11/12 lg:w-5/12 min-h-screen py-4 relative">
             <div className="bg-gradient-to-b from-[#F38300] absolute bottom-2 right-2 opacity-30 to-[#FF9900] rounded-xl w-full h-screen"></div>
             <div className="bg-gradient-to-b from-[#F38300] to-[#FF9900] rounded-xl w-full h-screen"></div>
           </aside>
