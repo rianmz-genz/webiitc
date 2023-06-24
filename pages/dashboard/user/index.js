a62c9289a884c9aa37f129465fb17d986e438873
rianmz-genz <adrianji1945@gmail.com>
2023-06-24 22:16:44 +0700

    add dashboard user view

---
+++ pages/dashboard/user/index.js
@@ -0,0 +1,27 @@
+import Cookies from "js-cookie";
+import React, { useEffect } from "react";
+
+export async function getServerSideProps(context) {
+  const token = context.req.cookies.token;
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  // Lanjutkan eksekusi jika token tersedia
+  // ...
+
+  return {
+    props: {},
+  };
+}
+
+const DashboardUser = () => {
+  return <div></div>;
+};
+
+export default DashboardUser;
