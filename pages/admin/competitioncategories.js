2d9c2525329629deeff826ee920f9ce9f27bca66
rianmz-genz <adrianji1945@gmail.com>
2023-06-26 17:27:11 +0700

    add modal new category

--- pages/admin/competitioncategories.js
+++ pages/admin/competitioncategories.js
@@ -1,53 +1,87 @@
 import { Button } from "@/components";
 import DashboardCard from "@/components/atoms/DashboardCard";
+import Input from "@/components/atoms/Input";
 import DashboardAdminTemplate from "@/components/pagetemplate/DashboardAdmin";
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
 import { BiHomeAlt } from "react-icons/bi";
 import { FiPlus } from "react-icons/fi";
 import { MdArrowForwardIos } from "react-icons/md";
 
 const CompetitionCategory = () => {
+  const [isModal, setIsModal] = useState(false);
   return (
-    <DashboardAdminTemplate title={"Categories"}>
-      <DashboardCard>
-        <ul className="flex items-center gap-2">
-          <p>
-            <BiHomeAlt className="text-gray-400" />
-          </p>
-          <p>
-            <MdArrowForwardIos className="text-xs text-gray-400" />
-          </p>
-          <p className="text-blue-600 text-sm">Lomba</p>
-        </ul>
-        <div className="flex justify-between items-center mt-2">
-          <h1 className="text-2xl fomt-semibold">Lomba</h1>
-          <Button
-            isSquare
-            color={"blue"}
-            additionals={"flex items-center gap-2"}
-          >
-            <FiPlus className="text-white" />
-            <p>Kategori Baru</p>
-          </Button>
-        </div>
-      </DashboardCard>
-      <DashboardCard>
-        <ul className="flex flex-col space-y-3">
-          <li className="shadow shadow-black/5 rounded-md px-8 py-4 flex justify-between items-center">
-            <p>Mahasiswa</p>
-            <div className="flex items-center space-x-2">
-              <button title="edit" className="bg-green-400/20 p-1 rounded">
-                <AiOutlineEdit className="text-green-400" />
-              </button>
-              <button title="hapus" className="bg-red/20 p-1 rounded">
-                <AiOutlineDelete className="text-red" />
-              </button>
-            </div>
-          </li>
-        </ul>
-      </DashboardCard>
-    </DashboardAdminTemplate>
+    <>
+      <div
+        className={`${
+          isModal ? "visible opacity-100" : "invisible opacity-0"
+        } transition-all duration-300 w-full h-screen fixed bg-black/5 backdrop-blur-sm z-50 flex justify-center items-center`}
+      >
+        <form className="w-full max-w-[500px] bg-white px-8 py-4 rounded-md">
+          <h1 className="text-xl mb-2">Tambah Kategori</h1>
+          <Input />
+          <div className="flex items-center space-x-2 mt-4 justify-end">
+            <Button
+              type="button"
+              onClick={() => setIsModal(false)}
+              isSquare
+              color={"bluelight"}
+            >
+              Batal
+            </Button>
+            <Button isSquare color={"blue"}>
+              Simpan
+            </Button>
+          </div>
+        </form>
+      </div>
+      <DashboardAdminTemplate title={"Categories"}>
+        <DashboardCard>
+          <ul className="flex items-center gap-2">
+            <p>
+              <BiHomeAlt className="text-gray-400" />
+            </p>
+            <p>
+              <MdArrowForwardIos className="text-xs text-gray-400" />
+            </p>
+            <p className="text-blue-600 text-sm">Lomba</p>
+          </ul>
+          <div className="flex justify-between items-center mt-2">
+            <h1 className="text-2xl fomt-semibold">Lomba</h1>
+            <Button
+              onClick={() => setIsModal(true)}
+              isSquare
+              color={"blue"}
+              additionals={"flex items-center gap-2"}
+            >
+              <FiPlus className="text-white" />
+              <p>Kategori Baru</p>
+            </Button>
+          </div>
+        </DashboardCard>
+        <DashboardCard>
+          <ul className="flex flex-col space-y-3">
+            <li className="shadow shadow-black/5 rounded-md px-8 py-4 flex justify-between items-center">
+              <p>Mahasiswa</p>
+              <div className="flex items-center space-x-2">
+                <button
+                  title="edit"
+                  className="bg-green-400/20 p-1 rounded hover:scale-95 transition-all duration-300"
+                >
+                  <AiOutlineEdit className="text-green-400" />
+                </button>
+                <button
+                  title="hapus"
+                  className="bg-red/20 p-1 rounded hover:scale-95 transition-all duration-300"
+                >
+                  <AiOutlineDelete className="text-red" />
+                </button>
+              </div>
+            </li>
+          </ul>
+        </DashboardCard>
+      </DashboardAdminTemplate>
+    </>
   );
 };
 
