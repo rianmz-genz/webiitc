156c11c9f5700f83b58b6f4e8569c453756da56e
rianmz-genz <adrianji1945@gmail.com>
2023-06-26 17:06:47 +0700

    responsive dashboard menu

--- components/pagetemplate/DashboardAdmin.js
+++ components/pagetemplate/DashboardAdmin.js
@@ -1,19 +1,35 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { BiCategoryAlt } from "react-icons/bi";
 import { CgEditBlackPoint } from "react-icons/cg";
 import { LuLayoutDashboard } from "react-icons/lu";
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FiX } from "react-icons/fi";
 const DashboardAdminTemplate = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <Head>
         <title>{title} - Admin</title>
       </Head>
-      <main className="bg-sslate w-full min-h-screen flex">
-        <nav className="w-full max-w-[250px] bg-white py-12 px-7 flex flex-col items-center justify-start">
+      <main className="bg-sslate w-full min-h-screen flex lg:flex-row flex-col">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="p-1 bg-blue-400/10 rounded-md fixed top-3 right-3 lg:hidden visible"
+        >
+          {isOpen ? (
+            <FiX className="text-2xl text-red" />
+          ) : (
+            <GiHamburgerMenu className="text-2xl text-blue-400" />
+          )}
+        </button>
+        <nav
+          className={`${
+            isOpen ? "block" : "lg:block hidden"
+          } w-full lg:max-w-[250px] bg-white py-12 px-7 flex flex-col items-center justify-start`}
+        >
           <Image
             className="w-24"
             src={"/images/LOGO/LOGO IIT COMPETITION PNG.png"}
