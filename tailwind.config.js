f2a73c6352e91d320cd6adeb0e84f7b1ff962eb3
rianmz-genz <adrianji1945@gmail.com>
2023-06-26 15:13:08 +0700

    add slate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'oren': '#F37F00',
-        'kuning': '#F7D708',
-        'softyellow': '#EACB05',
-        'dark': '#505050',
-        'silver': '#D9D9D9',
-        'red': '#F27641',
-        'darkblue': '#2E303F',
+        oren: "#F37F00",
+        kuning: "#F7D708",
+        softyellow: "#EACB05",
+        dark: "#505050",
+        silver: "#D9D9D9",
+        red: "#F27641",
+        darkblue: "#2E303F",
+        sslate: "#F8FAFC",
       },
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif']
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
