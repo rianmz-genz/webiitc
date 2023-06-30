40df5561e57c9d4e210b2a09700ce164bc819fb5
rianmz-genz <adrianji1945@gmail.com>
2023-06-30 17:39:36 +0700

    modals on submit

--- components/organisms/admin/Modals.js
+++ components/organisms/admin/Modals.js
@@ -2,14 +2,17 @@ import Button from "@/components/atoms/Button";
 import { Input } from "postcss";
 import React from "react";
 
-const Modals = ({ children, isModal }) => {
+const Modals = ({ children, isModal, onSubmit }) => {
   return (
     <div
       className={`${
         isModal ? "visible opacity-100" : "invisible opacity-0"
       } transition-all duration-300 w-full h-screen fixed bg-black/5 backdrop-blur-sm z-50 flex justify-center items-center`}
     >
-      <form className="w-11/12 lg:w-full max-w-[500px] bg-white px-8 py-4 rounded-md">
+      <form
+        onSubmit={onSubmit}
+        className="w-11/12 lg:w-full max-w-[500px] bg-white px-8 py-4 rounded-md"
+      >
         {children}
       </form>
     </div>
