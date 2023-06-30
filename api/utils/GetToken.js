a44b0d8d9fd363e3fedd5fca179774b993023ab7
rianmz-genz <adrianji1945@gmail.com>
2023-06-30 17:38:59 +0700

    get token utility

---
+++ api/utils/GetToken.js
@@ -0,0 +1,10 @@
+import Cookies from "js-cookie";
+
+const GetToken = ({ isAdmin = false }) => {
+  const token = Cookies.get(!isAdmin ? "token" : "adminKey");
+  console.log(token);
+  token ? token : "Unauthorized";
+  return `Bearer ${token}`;
+};
+
+export default GetToken;
