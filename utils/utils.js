587fcd8baccd77036f91b534aca4636c206ef1c4
rianmz-genz <adrianji1945@gmail.com>
2023-07-06 08:25:24 +0700

    add utility convert bool to binary

--- utils/utils.js
+++ utils/utils.js
@@ -1,17 +1,23 @@
 // joining class tailwind
 export function cn(...cns) {
-    return cns.join(' ');
-  }
+  return cns.join(" ");
+}
 
 // Variant builder
 export function variant(base, variants) {
-    return function builder(variantValues) {
-      const variantEntries = Object.entries(variantValues);
-      let variantClasses = [];
-      variantEntries.forEach(([name, value]) => {
-        variantClasses.push(variants[name][value]);
-      });
-      // console.log(...variantClasses, base)
-      return cn(base, ...variantClasses);
-    };
-  }
+  return function builder(variantValues) {
+    const variantEntries = Object.entries(variantValues);
+    let variantClasses = [];
+    variantEntries.forEach(([name, value]) => {
+      variantClasses.push(variants[name][value]);
+    });
+    // console.log(...variantClasses, base)
+    return cn(base, ...variantClasses);
+  };
+}
+
+export function getBinaryByBoolean({ boolean }) {
+  if (boolean) {
+    return 1;
+  } else return 0;
+}
