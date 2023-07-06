94c32849fa1b89cabafbebba77a043817c3db3aa
rianmz-genz <adrianji1945@gmail.com>
2023-07-06 08:24:51 +0700

    add competition

--- pages/admin/competition/add.js
+++ pages/admin/competition/add.js
@@ -1,83 +1,277 @@
+import GetAllCategoryApi from "@/api/admin/categories/GetAll";
+import CreateCompetitionApi from "@/api/admin/competition/Create";
 import { Button } from "@/components";
+import Alert from "@/components/atoms/Alert";
 import DashboardCard from "@/components/atoms/DashboardCard";
 import Dropdown from "@/components/atoms/Dropdown";
+import FileInput from "@/components/atoms/FilePond";
 import Text from "@/components/atoms/Text";
 import InputTitle from "@/components/molecules/InputTitle";
+import DynamicInput from "@/components/organisms/admin/DynamicInput";
+import PromptStyle from "@/components/organisms/admin/PromptStyle";
 import DashboardAdminTemplate from "@/components/pagetemplate/DashboardAdmin";
-import React from "react";
-import { AiOutlineEdit } from "react-icons/ai";
+import { getBinaryByBoolean } from "@/utils/utils";
+import React, { useState } from "react";
+import {
+  AiOutlineCheckCircle,
+  AiOutlineEdit,
+  AiOutlineLoading3Quarters,
+} from "react-icons/ai";
 import { BiHomeAlt } from "react-icons/bi";
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiXCircle } from "react-icons/fi";
+import { IoIosStats } from "react-icons/io";
 import { MdArrowForwardIos } from "react-icons/md";
 
-const AddCompetition = () => {
-  const dropdownOptions = [
-    { id: 1, value: "Pelajar & Mahasiswa" },
-    { id: 2, value: "Apa" },
-  ];
+export async function getServerSideProps() {
+  try {
+    const res = await GetAllCategoryApi();
+    if (res.status == 1) {
+      const categories = res.data?.categories;
+      return {
+        props: {
+          categories,
+        },
+      };
+    } else if (res.status == 0) {
+      return {
+        props: {
+          categories: [],
+        },
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        competitions: [],
+      },
+    };
+  }
+}
+const AddCompetition = ({ categories }) => {
+  const [stacks, setStacks] = useState([]);
+  const [juknis, setJuknis] = useState([]);
+  const [isIndividu, setIsIndividu] = useState(false);
+  const [cover, setCover] = useState(null);
+  const [name, setName] = useState("");
+  const [htm, setHtm] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [guidebook, setGuidebook] = useState("");
+  const [maxMembers, setMaxMembers] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [isHitApi, setIsHitApi] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isWrong, setIsWrong] = useState(false);
+  const [message, setMessage] = useState("");
+  const handleAddCategory = (e, id) => {
+    if (e) {
+      if (selectedCategories.includes(id)) return;
+      setSelectedCategories([...selectedCategories, id]);
+    } else {
+      setSelectedCategories(
+        selectedCategories.filter((category) => category != id)
+      );
+    }
+    console.log(selectedCategories);
+  };
+  const handleAddCompetition = () => {
+    setIsHitApi(true);
+    const data = {
+      cover,
+      name,
+      isIndividu: getBinaryByBoolean({ isIndividu }),
+      selectedCategories,
+      deadline,
+      maxMembers: parseInt(maxMembers),
+      price: htm,
+      techStack: stacks,
+      description,
+      guideBookLink: guidebook,
+      criteria: juknis,
+    };
+    console.log(data);
+    CreateCompetitionApi({
+      cover,
+      name,
+      isIndividu: getBinaryByBoolean({ isIndividu }),
+      selectedCategories,
+      deadline,
+      maxMembers: parseInt(maxMembers),
+      price: htm,
+      techStack: stacks,
+      description,
+      guideBookLink: guidebook,
+      criteria: juknis,
+    }).then((res) => {
+      console.log(res);
+      if (res.status == 1) {
+        setCover(null);
+        setName("");
+        setIsIndividu(false);
+        setSelectedCategories([]);
+        setDeadline("");
+        setMaxMembers("");
+        setHtm("");
+        setStacks([]);
+        setDescription("");
+        setGuidebook("");
+        setJuknis([]);
+        setMessage(res?.message);
+        setIsHitApi(false);
+      } else if (res.status == 0) {
+        setIsHitApi(false);
+        setMessage(res?.message);
+      }
+    });
+  };
+  const handleClickIndividu = (e) => {
+    const isChecked = e.target.checked;
+    setIsIndividu(isChecked);
+    if (isChecked) {
+      setMaxMembers("1");
+    } else {
+      setMaxMembers("");
+    }
+  };
   return (
-    <DashboardAdminTemplate>
-      <DashboardCard>
-        <ul className="flex items-center gap-2">
-          <p>
-            <BiHomeAlt className="text-gray-400" />
-          </p>
-          <p>
-            <MdArrowForwardIos className="text-xs text-gray-400" />
-          </p>
-          <p className="text-blue-600 text-sm">Lomba</p>
-          <p>
-            <MdArrowForwardIos className="text-xs text-gray-400" />
-          </p>
-          <p className="text-blue-600 text-sm">Buat</p>
-        </ul>
-        <div className="flex justify-between items-center mt-2">
-          <h1 className="text-2xl fomt-semibold">Buat Lomba</h1>
-          <Button
-            onClick={() => setIsModal(true)}
-            isSquare
-            color={"blue"}
-            additionals={"flex items-center gap-2"}
-          >
-            <p>Publish</p>
-          </Button>
-        </div>
-      </DashboardCard>
-      <div className="flex justify-center items-start w-11/12 mx-auto">
-        <DashboardCard className="w-full space-y-3 flex flex-col">
-          <div className="p-1 bg-slate-100 rounded-md w-fit">
-            <AiOutlineEdit className="text-xl" />
+    <div className="overflow-hidden">
+      <Alert onClose={() => setIsSuccess(false)} isOpen={isSuccess}>
+        <AiOutlineCheckCircle className="text-green-400 text-xl " />
+        <p>{message}</p>
+      </Alert>
+      <Alert onClose={() => setIsWrong(false)} isOpen={isWrong}>
+        <FiXCircle className="text-red text-xl " />
+        <p>{message}</p>
+      </Alert>
+      <DashboardAdminTemplate>
+        <DashboardCard>
+          <ul className="flex items-center gap-2">
+            <p>
+              <BiHomeAlt className="text-gray-400" />
+            </p>
+            <p>
+              <MdArrowForwardIos className="text-xs text-gray-400" />
+            </p>
+            <p className="text-blue-600 text-sm">Lomba</p>
+            <p>
+              <MdArrowForwardIos className="text-xs text-gray-400" />
+            </p>
+            <p className="text-blue-600 text-sm">Buat</p>
+          </ul>
+          <div className="flex justify-between items-center mt-2">
+            <h1 className="text-2xl fomt-semibold">Buat Lomba</h1>
+            <Button
+              onClick={() => handleAddCompetition()}
+              isSquare
+              color={"blue"}
+              additionals={"flex items-center gap-2"}
+            >
+              <p>
+                {isHitApi ? (
+                  <AiOutlineLoading3Quarters className="text-white animate-spin" />
+                ) : (
+                  "Publish"
+                )}
+              </p>
+            </Button>
           </div>
-          <InputTitle placeholder={"Nama lomba"} title={"Nama Lomba"} />
-          <Dropdown
-            title={"Kategori"}
-            options={dropdownOptions}
-            placeholder={"Pilih kategori"}
-          />
-          <InputTitle placeholder={"Harga lomba"} title={"HTM"} />
-          <InputTitle
-            placeholder={"Masukan jumlah anggota"}
-            title={"Total Anggota Per Tim"}
-          />
-          <label className="flex text-xs text-dark space-x-2">
-            <p>Lomba bersifat individu</p>
-            <input
-              type="checkbox"
-              onChange={(e) => console.log(e.target.checked)}
-            />
-          </label>
-          <label className=" text-dark">
-            <p>Deadline</p>
-            <input
-              type="date"
-              className="w-full px-4 py-2 border rounded-md mt-1 focus:ring-0 focus:outline-none"
-              placeholder="Tanggal"
-            />
-          </label>
         </DashboardCard>
-        <div className="w-full"></div>
-      </div>
-    </DashboardAdminTemplate>
+        <div className="flex justify-center items-start w-11/12 mx-auto lg:space-x-3 lg:flex-row flex-col">
+          <DashboardCard className="w-full space-y-3 flex flex-col">
+            <div className="p-1 bg-slate-100 rounded-md w-fit">
+              <AiOutlineEdit className="text-xl" />
+            </div>
+            <InputTitle
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              placeholder={"Nama lomba"}
+              title={"Nama Lomba"}
+            />
+            <div className="text-dark">
+              <p>Pilih kategori lomba</p>
+              <ul className="flex w-full flex-wrap gap-4 mt-1">
+                {categories?.map((category, index) => (
+                  <li key={index}>
+                    <label className="flex space-x-1">
+                      <input
+                        type="checkbox"
+                        onChange={(e) =>
+                          handleAddCategory(e.target.checked, category?.id)
+                        }
+                      />
+                      <p>{category?.name}</p>
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <InputTitle
+              value={htm}
+              onChange={(e) => setHtm(e.target.value)}
+              required
+              placeholder={"Harga lomba"}
+              title={"HTM"}
+            />
+            <InputTitle
+              value={maxMembers}
+              onChange={(e) => setMaxMembers(e.target.value)}
+              required
+              disabled={isIndividu}
+              placeholder={"Masukan jumlah anggota"}
+              title={"Total Anggota Per Tim"}
+            />
+            <label className="flex text-xs text-dark space-x-2">
+              <p>Lomba bersifat individu</p>
+              <input type="checkbox" onChange={(e) => handleClickIndividu(e)} />
+            </label>
+            <label className=" text-dark">
+              <p>Deadline</p>
+              <input
+                value={deadline}
+                onChange={(e) => setDeadline(e.target.value)}
+                type="date"
+                className="w-full px-4 py-2 border rounded-md mt-1 focus:ring-0 focus:outline-none"
+                placeholder="Tanggal"
+              />
+            </label>
+            <label className="text-dark">
+              <p className="mb-1">Tech Stack</p>
+              <PromptStyle keywords={stacks} setKeywords={setStacks} />
+            </label>
+            <label className="text-dark">
+              <p className="mb-1">Deskripsi</p>
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+                placeholder="Masukan Deskripsi"
+                className="border focus:outline-none rounded-md w-full  px-4 py-2"
+              ></textarea>
+            </label>
+            <InputTitle
+              value={guidebook}
+              onChange={(e) => setGuidebook(e.target.value)}
+              required
+              placeholder="Masukan link guidebook"
+              title={"Guidebook"}
+            />
+          </DashboardCard>
+          {/* RIGHT */}
+          <div className="w-11/12 mx-auto">
+            <p>Cover</p>
+            <FileInput image={cover} setImage={setCover} />
+            <DashboardCard className="w-full">
+              <div className="p-1 bg-slate-100 rounded-md w-fit">
+                <IoIosStats className="text-xl" />
+              </div>
+              <DynamicInput array={juknis} setArray={setJuknis} />
+            </DashboardCard>
+          </div>
+        </div>
+      </DashboardAdminTemplate>
+    </div>
   );
 };
 
