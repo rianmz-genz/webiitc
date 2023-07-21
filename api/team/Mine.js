094d51c8b8d94fd7f708ed505720abbb706b82fb
rianmz-genz <adrianji1945@gmail.com>
2023-07-21 10:34:08 +0700

    Get Competition Mine Api

---
+++ api/team/Mine.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import UrlTeam from "../routes/team";
+import GetToken from "../utils/GetToken";
+import { urlGetCompetitions } from "../routes/homepage/GetCompetitions";
+
+const GetMineTeam = async () => {
+  try {
+    const res = await axios({
+      method: "GET",
+      baseURL: `${urlGetCompetitions}/mine`,
+      headers: {
+        Authorization: GetToken({ isAdmin: false }),
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return error.response.data;
+  }
+};
+
+export default GetMineTeam;
