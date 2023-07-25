6e2dd90958a11695ef9e9bbde27ca0e93274f302
rianmz-genz <adrianji1945@gmail.com>
2023-07-25 21:15:37 +0700

    pay api

---
+++ api/payment/Pay.js
@@ -0,0 +1,26 @@
+import GetToken from "@/api/utils/GetToken";
+import axios from "axios";
+import UrlPayment from "../routes/payment";
+
+const PayApi = async ({ proveOfPayment, id }) => {
+  const data = {
+    proveOfPayment,
+  };
+  try {
+    const res = await axios({
+      method: "POST",
+      baseURL: `${UrlPayment}/${id}`,
+      data,
+      headers: {
+        Authorization: GetToken({ isAdmin: false }),
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return error.response.data;
+  }
+};
+export default PayApi;
