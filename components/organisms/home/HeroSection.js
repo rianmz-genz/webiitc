e5fd5c9f9828890a0e1e2ff8bfd72ffa528d68c9
Taufik-H <taufikrpl1@gmail.com>
2023-07-26 12:46:55 +0700

    hero ui

--- components/organisms/home/HeroSection.js
+++ components/organisms/home/HeroSection.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Text from "@/components/atoms/Text";
-import Button from "@/components/atoms/Button";
 import Navbar from "./Navbar";
 import Container from "@/components/molecules/Container";
 
@@ -9,33 +7,25 @@ const HeroSection = () => {
     <section id="hero" className="relative overflow-hidden h-screen shadow">
       <div className="absolute inset-0 ">
         <div
-          className=" w-full bg-cover  bg-no-repeat h-screen max-h-screen mt-20 bg-left opacity-70 lg:bg-center"
+          className="w-full bg-cover bg-no-repeat h-screen max-h-screen mt-20 bg-left  lg:bg-center"
           style={{
-            backgroundImage: "url('/images/Website/Artboard 1@4x.png')",
+            backgroundImage: "url('/images/Website/bghero.svg')",
           }}
         />
       </div>
-      <Container className="flex h-[90vh]  items-center justify-center">
+      <Container className="flex flex-col-reverse lg:flex-row h-[90vh] items-center justify-center z-10">
         <Navbar />
-        <Text
-          size={"bigtitle"}
-          weight={"bold"}
-          color={"black"}
-          additionals={
-            "leading-relaxed p-3 drop-shadow-xl text-4xl text-center mt-20  md:text-5xl lg:text-4xl md:w-11/12 lg:w-8/12"
-          }
-        >
-          <span className="text-transparent bg-clip-text bg-gradient-to-l from-orange-500 to bg-yellow-400">
-            {" "}
-            Buktikan{" "}
+
+        <p className="font-black text-slate-800 z-10 text-2xl lg:text-4xl leading-10 lg:w-6/12 text-center -mt-10">
+          <span className="text-transparent bg-gradient-to-tr from-orange-500 to bg-yellow-300 bg-clip-text">
+            Buktikan
           </span>{" "}
           bahwa kamu memiliki potensi emas dan{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-l from-orange-500 to bg-yellow-400">
-            {" "}
-            menangkan{" "}
+          <span className="text-transparent bg-gradient-to-tr from-orange-500 to bg-yellow-300 bg-clip-text">
+            menangkan
           </span>{" "}
           event ini
-        </Text>
+        </p>
       </Container>
     </section>
   );
