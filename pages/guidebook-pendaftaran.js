b9c7196688d25feb6a761e2d2c6deba83fa8e77d
rianmz-genz <adrianji1945@gmail.com>
2023-07-28 22:17:34 +0700

    embed

---
+++ pages/guidebook-pendaftaran.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function guidebookPendaftaran() {
+  return (
+    <iframe
+      src="https://scribehow.com/embed/Step-by-step_guide_Signing_up_and_joining_a_competition_on_IITC__rrJ9DfysTWe-A0Se9oppIA"
+      width="100%"
+      height="640"
+      allowfullscreen
+      frameborder="0"
+    ></iframe>
+  );
+}
