449003e4793881ca90162d5ce8a52393992a5c52
rianmz-genz <adrianji1945@gmail.com>
2023-07-29 18:20:02 +0700

    remove console

--- api/team/Join.js
+++ api/team/Join.js
@@ -16,10 +16,10 @@ const JoinApi = async ({ code }) => {
         Authorization: GetToken({ isAdmin: false }),
       },
     });
-    console.log(res.data);
+    //console.log(res.data);
     return res.data;
   } catch (error) {
-    console.log(error);
+    //console.log(error);
     return error.response.data;
   }
 };
