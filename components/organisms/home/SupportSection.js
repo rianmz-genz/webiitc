6e24299e97d7a1dab4ae7fabc122b2ead05ed30c
Taufik-H <taufikrpl1@gmail.com>
2023-08-01 15:31:42 +0700

    sponsor

--- components/organisms/home/SupportSection.js
+++ components/organisms/home/SupportSection.js
@@ -1,58 +1,50 @@
-import Image from 'next/image'
-import React from 'react'
-import { AiFillHeart } from 'react-icons/ai'
+import React from "react";
+import Slider from "react-slick";
+import { AiFillHeart } from "react-icons/ai";
+import Image from 'next/image';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const SupportSection = () => {
+  
+      
+        const settings = {
+    dots: true,
 
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 3000,
+ 
+      autoplaySpeed: 3000,
+      cssEase: "linear"
+  };
   return (
-    <div>
-      <div className="flex gap-3 justify-center my-10 capitalize items-center">
-      <AiFillHeart size={20} className='text-green-500'/>
-      Supported by :
-      </div>
-      {/* swiper */}
-      <div className="flex w-full p-5 justify-center items-center">
-      <div className="flex w-full flex-wrap items-center justify-center lg:justify-evenly gap-3"> 
-        <div className="w-32 h-10 relative py-5">
-          <Image
-            src={"/images/Sponsor/grab.png"}
-            alt="grab"
-            className='object-contain'
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-        <div className="w-32 h-16 relative">
-          <Image
-            src={"/images/Sponsor/endra.png"}
-            alt="endrapuri"
-            layout="fill"
-            className='object-contain'
-            objectFit="contain"
-          />
-        </div>
-        <div className="w-32 h-14 relative">
-          <Image
-            src={"/images/Sponsor/owb.png"}
-            alt="owabong"
-            layout="fill"
-            className='object-contain'
-            objectFit="contain"
-          />
-        </div>
-        <div className="w-32 h-16 relative">
-          <Image
-            src={"/images/Sponsor/pusko.png"}
-            alt="puskomedia"
-            layout="fill"
-            className='object-contain'
-            objectFit="contain"
-          />
-        </div>
-      </div>
-      </div>
+    <div className="overflow-hidden">
+      
+      <h2 className="flex items-center justify-center my-10 gap-3"><AiFillHeart className="text-green-500" /> Support by : </h2>
+     <div className="">
+      <Slider {...settings} className="">
+    <div className="">
+    <img src="/images/Sponsor/grab.png" alt=""className="h-16 object-contain " />
     </div>
-  )
+    <div className="">
+    <img src="/images/Sponsor/pusko.png" alt=""className="h-16 object-contain" />
+    </div>
+    <div className="">
+    <img src="/images/Sponsor/endra.png" alt=""className="h-16 object-contain   " />
+    </div>
+    <div className="">
+    <img src="/images/Sponsor/owb.png" alt=""className="h-16 object-contain  " />
+    </div>
+      </Slider>
+    </div>
+    </div>
+  );
 }
 
-export default SupportSection
+  
+
+export default SupportSection;
